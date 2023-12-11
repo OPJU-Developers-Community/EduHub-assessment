56fd6f7a84f8052aa59d5688b4ec2d765cee9655
PalakAgrawal020 <palakagrawal020@gmail.com>
2023-12-11 12:44:51 +0530

    Changed file structure and UI of login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Login from "@/organisms/Login";
+import Login from "./login";
 
 export default function Home() {
   return (
---
+++ src/pages/login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoginTemplate from '@/templates/LoginTemplate/LoginTemplate'
+
+export default function Login () {
+  return (
+    <LoginTemplate />
+  )
+}
