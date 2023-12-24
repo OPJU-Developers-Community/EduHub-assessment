5aaf0fc16a3a851d095fab47de65e39c9bacadfb
Subrat kumar jena <subratj611@gmail.com>
2023-12-25 02:03:08 +0530

    feat: 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import CommonTemplate from "@/templates/CommonTemplate/CommonTemplate";
+
+const Page404 = () => {
+  return (
+    <CommonTemplate>
+      <h1>hslh</h1>
+    </CommonTemplate>
+  );
+};
+
+export default Page404;
