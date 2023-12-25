85ff8137a89299db8cd910c227aa315759044660
Subrat kumar jena <subratj611@gmail.com>
2023-12-25 16:21:38 +0530

    question page template add

---
+++ src/pages/questions.js
@@ -0,0 +1,11 @@
+import CommonTemplate from "@/templates/CommonTemplate/CommonTemplate";
+
+const Questions = () => {
+  return (
+    <CommonTemplate>
+      <h1 className="text-2xl">Questions page</h1>
+    </CommonTemplate>
+  );
+};
+
+export default Questions;
