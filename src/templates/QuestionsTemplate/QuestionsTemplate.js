b825ccc6412bcf8a4619bcea7ca97fe3e2dfa4ce
Subrat kumar jena <subratj611@gmail.com>
2023-12-27 02:42:54 +0530

    fix: alignment for quick access

--- src/templates/QuestionsTemplate/QuestionsTemplate.js
+++ src/templates/QuestionsTemplate/QuestionsTemplate.js
@@ -21,7 +21,7 @@ const QuestionsTemplate = () => {
 
   return (
     <section className="mt-2">
-      <div className="flex flex-col justify-between md:flex-row md:items-center">
+      <div className="flex flex-col items-end justify-between md:flex-row md:items-center">
         <FilterTab tabs={questionPageFilterTab} />
         <Button
           className="text-white w-fit bg-indigo-600 focus:bg-indigo-600 hover:bg-indigo-700 px-3 mt-2 md:mt-0"
