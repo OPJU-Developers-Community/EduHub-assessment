9bc772cdd03c1cca67385d94340702b75e6fd036
Subrat kumar jena <subratj611@gmail.com>
2023-12-27 02:14:21 +0530

    fix(responsive): empty message text center

--- src/molecules/EmptyMessageBox/EmptyMessageBox.js
+++ src/molecules/EmptyMessageBox/EmptyMessageBox.js
@@ -5,7 +5,7 @@ const EmptyMessageBox = (props) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <EmptyBoxIcon className="w-48 h-36" />
-      <p className="text-lg text-gray-700">{message}</p>
+      <p className="text-center text-lg text-gray-700">{message}</p>
     </div>
   );
 };
