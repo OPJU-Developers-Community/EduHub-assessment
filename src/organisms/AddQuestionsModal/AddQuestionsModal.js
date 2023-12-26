c8b84fe1f65840535ae5732ef638b3cc74c835c0
Subrat kumar jena <subratj611@gmail.com>
2023-12-27 02:17:04 +0530

    create structure of adding question modal

---
+++ src/organisms/AddQuestionsModal/AddQuestionsModal.js
@@ -0,0 +1,9 @@
+const AddQuestionsModal = () => {
+  return (
+    <div className="fixed top-0 left-0 bg-modal-rgba w-screen h-screen">
+      AddQuestionsModal
+    </div>
+  );
+};
+
+export default AddQuestionsModal;
